f2cbd0f5a55f18ad8e89bba09230c5d6112be284
Changseo Jang <changseo.jang@mindslab.ai>
2022-08-09 09:25:06 +0900

    init:begin project

---
+++ app/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    compileSdk = 32
+
+    defaultConfig {
+        applicationId = "codes.beleap.wearos_pt_info"
+        minSdk = 30
+        targetSdk = 32
+        versionCode = 1
+        versionName = "1.0"
+
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            setProguardFiles(listOf("proguard-android-optimize.txt", "proguard-rules.pro"))
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    buildToolsVersion = "32.1.0-rc1"
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.8.0")
+    implementation("com.google.android.gms:play-services-wearable:17.1.0")
+    implementation("androidx.percentlayout:percentlayout:1.0.0")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
+}
