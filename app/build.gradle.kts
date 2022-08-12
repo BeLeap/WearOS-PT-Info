8d2b64e82360768c0732d384c9b85b6e89994526
Changseo Jang <changseo.jang@mindslab.ai>
2022-08-12 15:56:08 +0900

    release:v0.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "codes.beleap.wearos_pt_info"
         minSdk = 30
         targetSdk = 32
-        versionCode = 23
-        versionName = "0.1.2"
+        versionCode = 24
+        versionName = "0.1.3"
 
         buildConfigField("String", "SUBWAY_INFO_API_KEY",
             apiKeyProperties["SUBWAY_INFO_API_KEY"] as String
