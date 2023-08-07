1c615f49e9e8f2cbf5b757437cf53f5a23ff6e29
BeLeap <beleap@beleap.dev>
2023-08-07 18:47:20 +0900

    release: 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "codes.beleap.wearos_pt_info"
         minSdk = 30
         targetSdk = 33
-        versionCode = 39
-        versionName = "1.1.2"
+        versionCode = 40
+        versionName = "1.2.0"
 
         buildConfigField("String", "SUBWAY_INFO_API_KEY",
             apiKeyProperties["SUBWAY_INFO_API_KEY"] as String
